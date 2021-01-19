9bc3339e79e15912ec5b8f77ef4ef6ec2617bc66
richhh7g <richhh7g@protonmail.com>
2021-01-19 13:26:35 -0300

    chore: implementado a customização em tempo real

---
+++ inc/customizer/customizer.php
@@ -0,0 +1,15 @@
+<?php
+  if( is_customize_preview() ) {
+    /**
+     * Remove seções do customizador
+     *
+     * @since 0.3.2
+     * @return void
+     */
+    function remove_customize( $wp_customize ) {
+      $wp_customize->remove_section( 'static_front_page' ); // Remove a área de titulos padrao
+      $wp_customize->remove_section( 'custom_css' ); // Remove a customização de css
+    }
+    add_action( 'customize_register', 'remove_customize', 10 );
+  }
+?>
