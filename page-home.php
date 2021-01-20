8b8e97b391e462b91b3e895a966677ff3dca7f34
richhh7g <richhh7g@protonmail.com>
2021-01-19 21:41:05 -0300

    feature: seção de destaques criada

--- page-home.php
+++ page-home.php
@@ -4,6 +4,15 @@
   get_header();
 
   global $ka_uri;
+
+  $data = array();
+  $data['featured_products'] = sanitize_products( wc_get_products( array(
+        'limit'  => 12,
+        'featured' => true,
+        'orderBy' => 'date'
+      )
+    )
+  );
 ?>
 
 <?php if( get_theme_mod( 'banner-home-top-active' ) ) { ?>
@@ -24,20 +33,26 @@
 <?php if( get_theme_mod( 'banner-home-regua-active' ) ) { ?>
 <section class="banner-ruler">
   <span>
-    <img src="<?= $ka_uri; ?>assets/svgs/icons/safe.svg" alt="<?php esc_attr_e( 'Compra segura', 'kauabanga' ); ?>" />
+    <img src="<?= $ka_uri; ?>assets/svgs/icons/safe.svg" alt="<?php esc_attr_e( 'Compra segura', 'kauabanga' ); ?>"
+      draggable="false" />
     <?php _e( 'Sua compra é 100% segura', 'kauabanga' ); ?>
   </span>
   <span>
     <img src="<?= $ka_uri; ?>assets/svgs/icons/credit-card.svg"
-      alt="<?php esc_attr_e( 'Compre parcelado', 'kauabanga' ); ?>" />
+      alt="<?php esc_attr_e( 'Compre parcelado', 'kauabanga' ); ?>" draggable="false" />
     <?php _e( 'Parcele em até 12x sem juros', 'kauabanga' ); ?>
   </span>
   <span>
     <img src="<?= $ka_uri; ?>assets/svgs/icons/truck.svg"
-      alt="<?php esc_attr_e( 'Entrega por todo o Brasil', 'kauabanga' ); ?>" />
+      alt="<?php esc_attr_e( 'Entrega por todo o Brasil', 'kauabanga' ); ?>" draggable="false" />
     <?php _e( 'Entrega por todo o Brasil', 'kauabanga' ); ?>
   </span>
 </section>
 <?php } ?>
+
+<section class="ka-highlights container">
+  <h2 class="ka-title"><?php _e( 'Destaques', 'kauabanga' ); ?></h2>
+  <?php ka_product_list( $data['featured_products'] ); ?>
+</section>
 
 <?php get_footer(); ?>
