9fbe889b6ab2e0e8919d0b92112681d1d9a98f1c
richhh7g <richhh7g@protonmail.com>
2021-01-21 03:35:25 -0300

    chore: breadcrumb adicinado na pagina de busca

---
+++ woocommerce/archive-product.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<section class="breadcrumb">
+  <div class="row container"><?php woocommerce_breadcrumb( array( 'delimiter' => ' ↣ ') ); ?></div>
+</section>
+
+<?php get_footer(); ?>
