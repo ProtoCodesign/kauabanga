0da942fbb3505159d7d008bd2bbdda426b68c446
richhh7g <richhh7g@protonmail.com>
2021-01-21 14:35:42 -0300

    chore: javascript esta sendo carregado corretamente no wordpress

--- inc/kauabanga.php
+++ inc/kauabanga.php
@@ -16,6 +16,9 @@
   // Hook inicializa os estilos do tema
   add_action( 'wp_enqueue_scripts', 'ka_enqueue_styles' );
 
+  /* Carrega os scripts do tema */
+  add_action( 'wp_enqueue_scripts', 'ka_enqueue_scripts', 1 );
+
   /**
    * Registra estilos
    *
@@ -40,5 +43,20 @@
     wp_enqueue_style( 'ka-style');
   }
 
+  /**
+   * Inicia os scripts do tema
+   *
+   * @since 0.5.6
+   * @return void
+   */
+  function ka_enqueue_scripts() {
+
+    /* Adiciona o script principal do tema */
+    wp_enqueue_script( 'main', get_bundle_file( 'assets/js/dist/', 'index.*.js' ) , null, null, true );
+  }
+
   require_once( $ka_dir . 'inc/wc-functions.php' );
+
+  // Importa funções que serão usadas globalmente
+  require_once( $ka_dir . 'inc/utils/index.php' );
 ?>
---
+++ inc/utils/index.php
@@ -0,0 +1,5 @@
+<?php
+  global $ka_dir;
+
+  require_once( $ka_dir . 'inc/utils/get-bundle-file.php' );
+?>
