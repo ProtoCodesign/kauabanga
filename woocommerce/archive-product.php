26b0620f91a65c4570edb14466ba149ca47f5c8b
richhh7g <richhh7g@protonmail.com>
2021-01-21 14:52:54 -0300

    feature: produtos agora podem ser vistos na área de categoria e de busca

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,3 +1,16 @@
+<?php
+  global $ka_uri;
+
+  $data = array();
+  $products = array();
+
+  if( have_posts() ) : while( have_posts() ) : the_post();
+    $products[] = wc_get_product( get_the_ID() );
+  endwhile; endif;
+
+  $data['products'] = sanitize_products( $products );
+?>
+
 <?php get_header(); ?>
 
 <section class="breadcrumb" style="margin-top: 100px;">
@@ -44,6 +57,45 @@
       </span>
       <button type="submit" class="hidden"></button>
     </form>
+  </section>
+
+  <section class="showcase-archive col l9 xl9">
+    <section class="order-archive">
+      <?php woocommerce_catalog_ordering(); ?>
+    </section>
+
+    <div class="row">
+      <?php if( is_array( $data['products'] ) ) { ?>
+      <?php foreach( $data['products'] as $product ) { ?>
+      <div class="col l4 xl4">
+        <a href="<?= $product['link']; ?>" class="product">
+          <div class="image-product">
+            <img src="<?= $product['image']; ?>" alt="<?= $product['name']; ?>" draggable="false" />
+
+            <small
+              class="tag-discount"><?= 100 - (100 * $product['sale_price']) / $product['regular_price']; ?>%</small>
+
+            <small class="tag-category"><?= $product['category']; ?></small>
+          </div>
+
+          <div class="product-info">
+            <p class="product-name"><?= $product['name']; ?></p>
+
+            <span class="price">
+              <?php if( $product['price'] != $product['regular_price'] ) { ?>
+              <p class="discount-price"><?php _e( 'R$', 'kauabanga' ); ?>
+                <?= number_format( $product['regular_price'], 2, ",", "." ); ?></p>
+              <?php } ?>
+
+              <h4 class="current-price"><?php _e( 'R$', 'kauabanga' ); ?>
+                <?= number_format(  $product['price'], 2, ",", "." ); ?></h4>
+            </span>
+          </div>
+        </a>
+      </div>
+      <?php } } ?>
+
+    </div>
   </section>
 </main>
 
