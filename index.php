d045f9156174d929104b12b16db9f1ebb07a9aa5
richhh7g <richhh7g@protonmail.com>
2021-01-27 23:12:18 -0300

    feature: página de carrinho criada

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-  // Siléncio
+  get_header();
 ?>
+
+<?php if( is_cart() ) : ?>
+<section class="title-section">
+  <h2 class="ka-title" id="next-element"><?php _e( 'Carrinho', 'kauabanga' ); ?></h2>
+</section>
+<?php wc_get_template( 'cart/cart.php' ); ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
