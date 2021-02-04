a6d66f2d4b98b11c866699118bbc21a9306db5ca
richhh7g <richhh7g@protonmail.com>
2021-02-04 12:16:18 -0300

    feature: frete agora pode ser calculado na página de produto

--- inc/wc/hooks.php
+++ inc/wc/hooks.php
@@ -1,4 +1,10 @@
 <?php
+  if( class_exists( 'WC_Frenet_Shipping_Simulator' ) ) {
+    remove_action( 'woocommerce_single_product_summary', array( new WC_Frenet_Shipping_Simulator(), 'simulator' ), 40 );
+
+    add_action( 'woocommerce_before_add_to_cart_form', array( new WC_Frenet_Shipping_Simulator(), 'simulator' ) );
+  }
+
   add_filter( 'loop_shop_per_page', 'ka_products_per_page' );
 
   add_filter( 'woocommerce_account_menu_items', 'ka_remove_link_account' );
